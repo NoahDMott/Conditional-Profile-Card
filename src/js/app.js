d22927b8b43eb24d4da901e25440e1361baf9b25
NoahDMott <noahdmott@gmail.com>
2022-04-26 00:40:52 +0000

    conditional profile card

--- src/js/app.js
+++ src/js/app.js
@@ -28,19 +28,28 @@ function render(variables = {}) {
   // if includeCover==false then we reset the cover code without the <img> tag to make the cover transparent.
   let cover = `<div class="cover"><img src="${variables.background}" /></div>`;
   if (variables.includeCover == false) cover = "<div class='cover'></div>";
-
+  let namevar = variables.name == null ? "Enter name" : variables.name;
+  let namelast = variables.name == null ? "Enter name" : variables.name;
+  let rolevar = variables.role == null ? "Enter role" : variables.role;
+  let cityvar = variables.city == null ? "Enter city" : variables.city;
+  let countryvar =
+    variables.country == null ? "Enter country" : variables.country;
+  let socialmediabar =
+    variables.socialMediaPosition == "position-left"
+      ? "position-left"
+      : "position-right";
   // reset the website body with the new html output
   document.querySelector("#widget_content").innerHTML = `<div class="widget">
             ${cover}
           <img src="${variables.avatarURL}" class="photo" />
-          <h1>Lucy Boilett</h1>
-          <h2>Web Developer</h2>
-          <h3>Miami, USA</h3>
-          <ul class="position-right">
-            <li><a href="https://twitter.com/4geeksacademy"><i class="fab fa-twitter"></i></a></li>
-            <li><a href="https://github.com/4geeksacademy"><i class="fab fa-github"></i></a></li>
-            <li><a href="https://linkedin.com/4geeksacademy"><i class="fab fa-linkedin"></i></a></li>
-            <li><a href="https://instagram.com/4geeksacademy"><i class="fab fa-instagram"></i></a></li>
+          <h1>${namevar} ${namelast}</h1>
+          <h2>${rolevar}</h2>
+          <h3>${cityvar}, ${countryvar}</h3>
+          <ul class="${socialmediabar}">
+            <li><a href="${variables.twitter}"><i class="fab fa-twitter"></i></a></li>
+            <li><a href="${variables.github}"><i class="fab fa-github"></i></a></li>
+            <li><a href="${variables.linkedin}"><i class="fab fa-linkedin"></i></a></li>
+            <li><a href="${variables.instagram}"><i class="fab fa-instagram"></i></a></li>
           </ul>
         </div>
     `;
